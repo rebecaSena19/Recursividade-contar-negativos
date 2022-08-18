bdfe1a290a85e0e3bb089d349c5a3e13918aae73
rebecaSena19 <111463921+rebecaSena19@users.noreply.github.com>
2022-08-18 11:15:52 -0300

    Add files via upload

---
+++ src/controller/Recursnumnegat.java
@@ -0,0 +1,20 @@
+package controller;
+
+public class Recursnumnegat {
+
+	public Recursnumnegat () {
+
+		super();
+	}
+	//ponto de parada
+	public int vetNegativos (int[] vet, int cont ) {
+		if (cont == 0) {
+			return 0;
+		}
+    //função do termo anterior
+		if (vet[cont - 1] < 0) {
+			return 1+vetNegativos (vet, cont - 1);
+		}
+		return vetNegativos ( vet, cont - 1);
+	}
+}
---
+++ src/view/Principal.java
@@ -0,0 +1,19 @@
+//exercício feito em conjunto com outra aluna
+
+package view;
+
+import controller.Recursnumnegat;
+
+ public class Principal {
+
+  public static void main( String[] args){
+
+  Recursnumnegat Negativ = new Recursnumnegat ();
+
+ int[] vet = {2, -1, -4, 1, 7, -5, -1};
+
+ int resultado = Negativ.vetNegativos.vet.length;
+
+ System.out.println(resultado);
+   }
+}
